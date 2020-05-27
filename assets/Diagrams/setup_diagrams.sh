f65911698fccc2fea63046bd33c902361cd9d8d1
fortuna <fortuna@devgrid.co.uk>
2020-05-27 16:43:35 -0300

    Update diagram

--- assets/Diagrams/setup_diagrams.sh
+++ assets/Diagrams/setup_diagrams.sh
@@ -2,7 +2,7 @@
 
 for svg_file in $(ls *.svg)
 do
-	new_file="../3gppmap/_includes/${svg_file%svg}html"
+	new_file="../../_includes/${svg_file%svg}html"
   echo "Parsing $svg_file into $new_file"
 	cat $svg_file | grep -v "DOCTYPE" > $new_file
 done
