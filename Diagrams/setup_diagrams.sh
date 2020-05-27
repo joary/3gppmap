4b72f2788d715002eeae67aefd1b308818feff50
fortuna <fortuna@devgrid.co.uk>
2020-05-27 12:18:27 -0300

    Initial map digram

---
+++ Diagrams/setup_diagrams.sh
@@ -0,0 +1,8 @@
+
+
+for svg_file in $(ls *.svg)
+do
+	new_file="../3gppmap/_includes/${svg_file%svg}html"
+  echo "Parsing $svg_file into $new_file"
+	cat $svg_file | grep -v "DOCTYPE" > $new_file
+done
